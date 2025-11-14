8d129faab9bebd3c80835b71e95bc4fb923038b3
John Henard Salango Fernandez <henardsalango09@gmail.com>
2025-11-14 15:27:44 +0100

    first commit

---
+++ app/src/main/java/com/example/intro_proyecto_dam2/MainActivity.kt
@@ -0,0 +1,59 @@
+    package com.example.intro_proyecto_dam2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.intro_proyecto_dam2.ui.theme.Intro_proyecto_DAM2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            /*
+            Intro_proyecto_DAM2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+            */
+            Inicio()
+        }
+    }
+}
+
+
+@Composable
+fun Inicio() {
+    Column() {
+        Text(text = "Hello World")
+    }
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Intro_proyecto_DAM2Theme {
+        Greeting("Android")
+    }
+}
